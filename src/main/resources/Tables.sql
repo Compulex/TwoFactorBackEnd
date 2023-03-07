798ad5daa57c2d313a04ce584d5934093a0025f0
compulex <alexalewis2013@afscholars.org>
2023-03-07 12:20:00 -0500

    drop table first

--- src/main/resources/Tables.sql
+++ src/main/resources/Tables.sql
@@ -1,6 +1,8 @@
 --h2 is typically used to setup a test database, not a prod database.
 --first, drop your tables (to reset your database for testing)
 --then create your tables
+drop table User;
+
 create table User(
     id int primary key,
     username varchar(255),
