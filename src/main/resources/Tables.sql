d7616a7f52e8f61d0087d284f3bd3bceb2baa798
compulex <alexalewis2013@afscholars.org>
2023-03-07 14:54:26 -0500

    directories added

--- src/main/resources/Tables.sql
+++ src/main/resources/Tables.sql
@@ -1,9 +1,6 @@
---h2 is typically used to setup a test database, not a prod database.
---first, drop your tables (to reset your database for testing)
---then create your tables
-drop table User;
+drop table UserData;
 
-create table User(
+create table UserData(
     id int primary key,
     username varchar(255),
     password varchar(255),
--- target/classes/Tables.sql
+++ target/classes/Tables.sql
@@ -1,4 +1,9 @@
---h2 is typically used to setup a test database, not a prod database.
---first, drop your tables (to reset your database for testing)
---then create your tables
-afdbfdb
+drop table UserData;
+
+create table UserData(
+    id int primary key,
+    username varchar(255),
+    password varchar(255),
+    code varchar(255)
+);
+
