3548d4a5cf1fd95de28117ec4da6311d3887cfd2
Joseph Dabreu <joeydabreu@gmail.com>
2023-03-07 16:16:46 -0500

    Added code model to project

---
+++ src/main/java/Model/Code.java
@@ -0,0 +1,16 @@
+package Model;
+
+public class Code {
+    public int generatedCode;
+
+    public int getGeneratedCode()
+    {
+        return generatedCode;
+    }
+
+    public void setGeneratedCode(int generatedCode)
+    {
+        this.generatedCode = generatedCode;
+    }
+
+}
