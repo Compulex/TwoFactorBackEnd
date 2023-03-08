79697346292e46a47a3e22a854eaa43501d2509d
Joseph Dabreu <joeydabreu@gmail.com>
2023-03-08 14:18:51 -0500

    Added and modified controller to test user

--- src/main/java/Controller/TwoFactorController.java
+++ src/main/java/Controller/TwoFactorController.java
@@ -1,4 +1,5 @@
 package Controller;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
@@ -10,6 +11,35 @@ import io.javalin.http.Context;
 
 import java.util.List;
 
-public class Controller {
-    
+public class TwoFactorController {
+    UserService userService;
+    //Code service
+
+    public TwoFactorController()
+    {
+        this.userService = new UserService();
+    }
+
+    public Javalin startAPI() {
+        Javalin app = Javalin.create();
+        app.post("/register", this::postUserHandler);
+        return app;
+    }
+
+    private void postUserHandler(Context ctx)throws JsonProcessingException {
+        ObjectMapper om = new ObjectMapper();
+        User user = om.readValue(ctx.body(), User.class);
+        User insertedUser = userService.addUser(user);
+
+        if (user.username == null)
+        {
+            ctx.json(om.writeValueAsString(insertedUser));
+            ctx.status(200);
+        }
+
+        else{
+            ctx.status(400);
+        }
+    }
+
 }
