83739aadfe212a77c7f7fb7d251cfd414632915c
Joseph Dabreu <joeydabreu@gmail.com>
2023-03-08 14:17:34 -0500

    added maven to compiler

--- src/main/java/Controller/Controller.java
+++
@@ -1,9 +0,0 @@
-package Controller;
-
-import Model.User;
-import Model.Code;
-
-import java.util.List;
-
-public class Controller {
-}
---
+++ src/main/java/Controller/TwoFactorController.java
@@ -0,0 +1,15 @@
+package Controller;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import Model.User;
+import Model.Code;
+import Service.UserService;
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+
+import java.util.List;
+
+public class Controller {
+    
+}
